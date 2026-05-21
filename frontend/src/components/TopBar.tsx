cbf75428bd8ef5b94c6c9939e433a08b2935eebe
M1K45 <272570@student.pwr.edu.pl>
2026-05-21 10:08:39 +0200

    refactor: top bar now sticks to the top

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,38 +1,39 @@
-import { Box, Container, IconButton, Typography } from "@mui/material"
+import { Box, AppBar, IconButton, Typography, Toolbar } from "@mui/material"
 import logo from '../assets/logo1.png'
 import { useNavigate } from "react-router-dom";
 
 export const TopBar = () => {
     const navigate = useNavigate();
     return (
-        <Container sx={{
-            display: "flex",
-            alignItems: "center",
-            gap: "15px",
-            padding: 0,
-            backgroundColor: "lightgray"
-
-        }}>
-            <IconButton
-                onClick={() => navigate("/")}
-            >
-                <img
-                    src={logo}
-                    alt="Science club logo"
-                    style={{
-                        height: '30px',
-                        width: 'auto',
-                    }}
-                />
-            </IconButton>
-            <Box sx={{
-                display: "flex",
-                justifyContent: "center",
-                width: "100%",
+        <AppBar
+            position="sticky"
+            sx={{
+                backgroundColor: "lightgray",
+                color: "black"
             }}>
-                <Typography>Smart Home</Typography>
-            </Box>
-        </Container>
+            <Toolbar sx={{ display: "flex", gap: "15px", padding: 0 }}>
+                <IconButton
+                    onClick={() => navigate("/")}
+                >
+                    <img
+                        src={logo}
+                        alt="Science club logo"
+                        style={{
+                            height: '30px',
+                            width: 'auto',
+                        }}
+                    />
+                </IconButton>
+                <Box sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    width: "100%",
+                }}>
+                    <Typography>Smart Home</Typography>
+                </Box>
+            </Toolbar>
+
+        </AppBar>
     )
 }
 
